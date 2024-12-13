c9dc93dee15682cf9463a6513d4189d25f9d40ba
mohanddo <mohandmeghaoui0@gmail.com>
2024-12-13 21:29:10 +0100

    Added search bar in the eventdetails component

--- src/app/(routes)/eventdetails.tsx
+++ src/app/(routes)/eventdetails.tsx
@@ -1,155 +1,204 @@
-import { useLocalSearchParams } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { View, Text, Pressable, ScrollView } from 'react-native';
-import { Entypo, FontAwesome6, MaterialCommunityIcons } from '@expo/vector-icons';
-import { router } from 'expo-router';
-import Button from '@/components/global/Button';
-import { colors } from '@/constants/colors';
+import Button from "@/components/global/Button";
+import { colors } from "@/constants/colors";
+import {
+  Entypo,
+  FontAwesome6,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
+import { SearchBar } from "@rneui/themed";
+import { router, useLocalSearchParams } from "expo-router";
+import { useState } from "react";
+import { Pressable, ScrollView, Text, View, ViewStyle } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function EventDetails() {
-    const participants = [
-        { id: 1, name: 'John Doe', status: 'Present' },
-        { id: 2, name: 'Jane Doe', status: 'Pending' },
-        { id: 3, name: 'John Smith', status: 'Absent' },
-        { id: 4, name: 'Jane Smith', status: 'Present' },
-        { id: 5, name: 'John Doe', status: 'Present' },
-        { id: 6, name: 'Jane Doe', status: 'Pending' },
-        { id: 7, name: 'John Smith', status: 'Absent' },
-        { id: 8, name: 'Jane Smith', status: 'Present' },
-        { id: 9, name: 'John Doe', status: 'Present' },
-        { id: 10, name: 'Jane Doe', status: 'Pending' },
-        { id: 11, name: 'John Smith', status: 'Absent' },
-        { id: 12, name: 'Jane Smith', status: 'Present' },
-        { id: 13, name: 'John Doe', status: 'Present' },
-        { id: 14, name: 'Jane Doe', status: 'Pending' },
-        { id: 15, name: 'John Smith', status: 'Absent' },
-        { id: 16, name: 'Jane Smith', status: 'Present' },
-        { id: 17, name: 'John Doe', status: 'Present' },
-        { id: 18, name: 'Jane Doe', status: 'Pending' },
-        { id: 19, name: 'John Smith', status: 'Absent' },
-        { id: 20, name: 'Jane Smith', status: 'Present' },
-        { id: 21, name: 'John Doe', status: 'Present' },
-        { id: 22, name: 'Jane Doe', status: 'Pending' },
+  const participants = [
+    { id: 1, name: "John Doe", status: "Present" },
+    { id: 2, name: "Jane Doe", status: "Pending" },
+    { id: 3, name: "John Smith", status: "Absent" },
+    { id: 4, name: "Jane Smith", status: "Present" },
+    { id: 5, name: "John Doe", status: "Present" },
+    { id: 6, name: "Jane Doe", status: "Pending" },
+    { id: 7, name: "John Smith", status: "Absent" },
+    { id: 8, name: "Jane Smith", status: "Present" },
+    { id: 9, name: "John Doe", status: "Present" },
+    { id: 10, name: "Jane Doe", status: "Pending" },
+    { id: 11, name: "John Smith", status: "Absent" },
+    { id: 12, name: "Jane Smith", status: "Present" },
+    { id: 13, name: "John Doe", status: "Present" },
+    { id: 14, name: "Jane Doe", status: "Pending" },
+    { id: 15, name: "John Smith", status: "Absent" },
+    { id: 16, name: "Jane Smith", status: "Present" },
+    { id: 17, name: "John Doe", status: "Present" },
+    { id: 18, name: "Jane Doe", status: "Pending" },
+    { id: 19, name: "John Smith", status: "Absent" },
+    { id: 20, name: "Jane Smith", status: "Present" },
+    { id: 21, name: "John Doe", status: "Present" },
+    { id: 22, name: "Jane Doe", status: "Pending" },
+  ];
 
-    ];
+  const { event: paramsEvent } = useLocalSearchParams();
+  const event = JSON.parse(paramsEvent as string);
 
-    const { event: paramsEvent } = useLocalSearchParams();
-    const event = JSON.parse(paramsEvent as string);
+  const [participantsFilter, setParticipantsFilter] = useState("");
+  const [filtredParticipants, setfiltredParticipants] = useState(participants);
 
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
-            <Pressable
-                style={{ flexDirection: 'row', alignItems: 'center', gap: 5, padding: 10 }}
-                onPress={() => router.back()}
-            >
-                <Entypo name="chevron-left" size={32} color={colors.black} />
-                <Text style={{ fontSize: 24, fontFamily: 'Poppins-SemiBold' }}>
-                    {event.name}
-                </Text>
-            </Pressable>
-            <ScrollView
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={{
-                    width: '100%',
-                    height: "auto",
-                    marginBottom: 40,
-                    paddingHorizontal: 20,
-                    paddingVertical: 10,
-                    flexDirection: 'column',
-                    gap: 10
-                }}
-            >
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <FontAwesome6 name="location-crosshairs" size={24} color={colors.black} />
-                    <Text
-                        style={{
-                        fontFamily: 'Poppins-SemiBold',
-                        fontSize: 16,
-                        marginLeft: 5,
-                        flexShrink: 1,
-                        flexWrap: 'wrap',
-                        }}
-                    >
-                        Location: {event.location}
-                    </Text>
-                </View>
-
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <MaterialCommunityIcons name="calendar" size={25} color={colors.black} />
-                    <Text
-                        style={{
-                        fontFamily: 'Poppins-SemiBold',
-                        fontSize: 16,
-                        marginLeft: 5,
-                        flexShrink: 1,
-                        flexWrap: 'wrap',
-                        }}
-                    >
-                        Date: {event.date}
-                    </Text>
-                </View>
-                <Button 
-                    title="Start Check-In" onPress={() => router.push({
-                        pathname: '/(routes)/scaneventcode',
-                        params: { event_id: event.id.toString() },
-                        })
-                    }
-                    //disabled={event.date < new Date()}
-                />
-                <View
-                    style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Text style={{ fontSize: 18, fontFamily: 'Poppins-SemiBold' }}>
-                        Participant
-                    </Text>
-                    <Text style={{ fontSize: 18, fontFamily: 'Poppins-SemiBold' }}>
-                        Status
-                    </Text>
-                </View>
-                <View
-                    style={{
-                    height: 2,
-                    width: '100%',
-                    backgroundColor: colors.black,
-                    borderRadius: 20,
-                    alignSelf: 'center',
-                    marginVertical: -4,
-                    }}
-                />
-                <View
-                    style={{
-                        height: "auto",
-                    }}
-                >
-                    {participants.map((participant) => (
-                        <View
-                            key={participant.id}
-                            style={{
-                                flexDirection: 'row',
-                                justifyContent: 'space-between',
-                                alignItems: 'center',
-                                marginBottom: 10,
-                            }}
-                        >
-                            <Text style={{ fontSize: 16, fontFamily: 'Poppins-SemiBold' }}>
-                                {participant.name}
-                            </Text>
-                            <Text
-                                style={{
-                                    fontSize: 16,
-                                    fontFamily: 'Poppins-SemiBold',
-                                    color: participant.status === 'Present' ? 'green' : participant.status === 'Absent' ? 'red' : colors.black,
-                                }}>
-                                {participant.status}
-                            </Text>
-                        </View>
-                    ))}
-                </View>
-            </ScrollView>
-        </SafeAreaView>
+  const filterParticipants = (newValue: string) => {
+    setParticipantsFilter(newValue);
+    setfiltredParticipants(
+      participants.filter((participant) =>
+        participant.name.toLowerCase().includes(newValue.toLowerCase())
+      )
     );
+  };
+
+  const searchBarContainerStyle: ViewStyle = {
+    backgroundColor: "black",
+    borderRadius: 10,
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
+      <Pressable
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 5,
+          padding: 10,
+        }}
+        onPress={() => router.back()}
+      >
+        <Entypo name="chevron-left" size={32} color={colors.black} />
+        <Text style={{ fontSize: 24, fontFamily: "Poppins-SemiBold" }}>
+          {event.name}
+        </Text>
+      </Pressable>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          width: "100%",
+          height: "auto",
+          marginBottom: 40,
+          paddingHorizontal: 20,
+          paddingVertical: 10,
+          flexDirection: "column",
+          gap: 10,
+        }}
+      >
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <FontAwesome6
+            name="location-crosshairs"
+            size={24}
+            color={colors.black}
+          />
+          <Text
+            style={{
+              fontFamily: "Poppins-SemiBold",
+              fontSize: 16,
+              marginLeft: 5,
+              flexShrink: 1,
+              flexWrap: "wrap",
+            }}
+          >
+            Location: {event.location}
+          </Text>
+        </View>
+
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <MaterialCommunityIcons
+            name="calendar"
+            size={25}
+            color={colors.black}
+          />
+          <Text
+            style={{
+              fontFamily: "Poppins-SemiBold",
+              fontSize: 16,
+              marginLeft: 5,
+              flexShrink: 1,
+              flexWrap: "wrap",
+            }}
+          >
+            Date: {event.date}
+          </Text>
+        </View>
+        <Button
+          title="Start Check-In"
+          onPress={() =>
+            router.push({
+              pathname: "/(routes)/scaneventcode",
+              params: { event_id: event.id.toString() },
+            })
+          }
+          //disabled={event.date < new Date()}
+        />
+
+        <SearchBar
+          placeholder="Search Participant"
+          onChangeText={filterParticipants}
+          value={participantsFilter}
+        />
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: 18, fontFamily: "Poppins-SemiBold" }}>
+            Participant
+          </Text>
+          <Text style={{ fontSize: 18, fontFamily: "Poppins-SemiBold" }}>
+            Status
+          </Text>
+        </View>
+        <View
+          style={{
+            height: 2,
+            width: "100%",
+            backgroundColor: colors.black,
+            borderRadius: 20,
+            alignSelf: "center",
+            marginVertical: -4,
+          }}
+        />
+        <View
+          style={{
+            height: "auto",
+          }}
+        >
+          {filtredParticipants.map((participant) => (
+            <View
+              key={participant.id}
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginBottom: 10,
+              }}
+            >
+              <Text style={{ fontSize: 16, fontFamily: "Poppins-SemiBold" }}>
+                {participant.name}
+              </Text>
+              <Text
+                style={{
+                  fontSize: 16,
+                  fontFamily: "Poppins-SemiBold",
+                  color:
+                    participant.status === "Present"
+                      ? "green"
+                      : participant.status === "Absent"
+                      ? "red"
+                      : colors.black,
+                }}
+              >
+                {participant.status}
+              </Text>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,24 @@
-import { useState } from 'react';
-import { View, Text, Image } from 'react-native';
-import FormInput from '@/components/form/FormInput';
-import { KeyboardAvoidingView, ScrollView } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { MaterialIcons } from '@expo/vector-icons';
-import Button from '@/components/global/Button';
-import { router } from 'expo-router';
-import { colors } from '@/constants/colors';
-import axios from 'axios';
-import { Toast } from 'react-native-toast-notifications';
+import "@/../global.css";
+import FormInput from "@/components/form/FormInput";
+import Button from "@/components/global/Button";
+import { colors } from "@/constants/colors";
+import { MaterialIcons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { useState } from "react";
+import {
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Toast } from "react-native-toast-notifications";
+import "../../global.css";
 
 export default function Login() {
-
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async () => {
     try {
@@ -22,32 +27,33 @@ export default function Login() {
         username,
         password,
       });*/
-      if(true) { // If login is successful
+      if (true) {
+        // If login is successful
         Toast.show("Logged in successfully, welcome back!", {
           placement: "bottom",
           type: "success",
-        })
+        });
         router.replace("/(authed)/home");
       } else {
-        Toast.show("The credentials you entered are wrong!", {
-          placement: "bottom",
-        })
       }
     } catch (error) {
       console.error(error);
     }
     router.replace("/(authed)/home");
-  }
+  };
 
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: colors.white,}}>
-      <KeyboardAvoidingView style={{flex: 1}}>
-        <ScrollView style={{flex: 1}} contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
+      <KeyboardAvoidingView style={{ flex: 1 }}>
+        <ScrollView
+          style={{ flex: 1 }}
+          contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}
+        >
           <View
             style={{
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
               padding: 40,
               gap: 40,
               flex: 1,
@@ -56,26 +62,32 @@ export default function Login() {
             <View
               style={{
                 gap: 10,
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
                 marginBottom: 20,
-              }}      
+              }}
             >
-              <Text style={{
-                fontSize: 40,
-                fontWeight: 'bold',
-              }}>
+              <Text
+                style={{
+                  fontSize: 40,
+                  fontWeight: "bold",
+                }}
+              >
                 InCheck
               </Text>
-              <MaterialIcons name="qr-code-scanner" size={60} color={colors.black} />
+              <MaterialIcons
+                name="qr-code-scanner"
+                size={60}
+                color={colors.black}
+              />
             </View>
 
             <View
               style={{
-                flexDirection: 'column',
+                flexDirection: "column",
                 gap: 30,
-                width: '100%',
+                width: "100%",
               }}
             >
               <FormInput
@@ -96,7 +108,10 @@ export default function Login() {
 
             <Button title="Login" onPress={handleSubmit} />
 
-            <Image source={require('@/assets/images/gdg.png')} style={{ width: 100, height: 50, marginTop: 10 }} />
+            <Image
+              source={require("@/assets/images/gdg.png")}
+              style={{ width: 100, height: 50, marginTop: 10 }}
+            />
           </View>
         </ScrollView>
       </KeyboardAvoidingView>
